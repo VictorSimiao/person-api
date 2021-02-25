70a5221352ea3007537d3f01bb6edf8a0037ea27
Victor Simiao <victor.sreis@hotmail.com>
2021-02-25 09:21:30 -0300

    Iniciando a camada de Controller

---
+++ src/main/java/br/com/victorreis/personapi/controller/PessoaController.java
@@ -0,0 +1,9 @@
+package br.com.victorreis.personapi.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/pessoas")
+public class PessoaController {
+}
